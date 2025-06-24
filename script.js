f620aca4ad9d6d883eac4aecd5e7a52fa90fcf2a
Rahul <ankanarahul@gmail.com>
2025-06-24 20:57:10 +0530

    Initial commit - Portfolio website

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+
+
+document.getElementById('toggleTheme').addEventListener('click', function () {
+  document.body.classList.toggle('dark-mode');
+});
+
+// Change about text
+/*document.getElementById('changeAbout').addEventListener('click', function () {
+  const about = document.getElementById('about-text');
+  about.textContent = "Motivated 2023 CSE graduate with a strong foundation in Java, SQL, and Web Development. Seeking real-world opportunities to contribute and learn.";
+});*/
+
+
+document.getElementById('alertMe').addEventListener('click', function () {
+  alert("Thank you for visiting Ankana Rahul Naidu's portfolio!");
+});
